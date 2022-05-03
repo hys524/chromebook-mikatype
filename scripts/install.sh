c35c39f8df834a58c39ed9d8bbc9247844b9a1c3
Atsushi Hayashi <vi.rago524@gmail.com>
2022-05-04 02:13:13 +0900

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo -e "\e[36m▼ソフトウェアをアップデート・インストールします\e[m"
+echo -e "\e[36m▼ソフトウェアをインストールします\e[m"
 sudo apt-get update
-sudo apt-get -y install unzip default-jre default-jdk
+sudo apt-get -y install unzip default-jre default-jdk imagemagick
 
 echo -e "\e[36m▼mikatypeディレクトリを作成します\e[m"
 mkdir -pv ${HOME}/mikatype
@@ -17,8 +17,18 @@ unzip mikatype_java.zip
 echo -e "\e[36m▼MIKATYPE.javaをコンパイルします\e[m"
 javac MIKATYPE.java
 
+echo -e "\e[36m▼アプリアイコンを変換します\e[m"
+convert mikatype.ico mikatype.png
+
 echo -e "\e[36m▼デスクトップファイルを作成します\e[m"
 mkdir -pv ${HOME}/.local/share/applications/
-echo -e "[Desktop Entry]\nName=MIKATYPE\nExec=java -cp ${HOME}/mikatype MIKATYPE\nType=Application" > ${HOME}/.local/share/applications/mikatype.desktop
+echo -e "[Desktop Entry]\n\
+Type=Application\n\
+Name=MIKATYPE\n\
+Comment=美佳のタイプトレーナ\n\
+Exec=java -cp ${HOME}/mikatype MIKATYPE\n\
+Icon=${HOME}/mikatype/mikatype-0.png\n\
+Terminal=false" > ${HOME}/.local/share/applications/mikatype.desktop
+echo -e "${HOME}/.local/share/applications/mikatype.desktop"
 
 echo -e "\e[36mスクリプト終了\e[m"
